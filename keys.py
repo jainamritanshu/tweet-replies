ddc7281d05d1cce5f4eb355291d6881593b9bbaa
jainamritanshu <jainamritanshu@gmail.com>
2016-04-25 03:10:16 +0530

    keys done

---
+++ keys.py
@@ -0,0 +1,7 @@
+#Get your keys from twitter api by visiting dev.twitter.com
+keys = dict(
+consumer_key = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+consumer_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+access_token = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+) #keys stored in a dict
